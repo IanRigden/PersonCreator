d4465d94f02f0f9b1dbc305a8b7ee6a75f7fb3b9
Ian Rigden <81633596+IanRigden@users.noreply.github.com>
2023-02-06 14:36:46 +0000

    Update PersonCreator.java

--- src/PersonCreator.java
+++ src/PersonCreator.java
@@ -1,17 +1,19 @@
 public class PersonCreator {
     public static void main(String[] args) {
         //Create objects
-        Person Person01 = new Person("James Peterson", 34, "180cm", "73.5lbs");
-        Person Person02 = new Person("Bob Thornley", 25, "171cm", "68.2lbs");
+        Person Person01 = new Person("James Peterson", 34, 180, 73.5);
+        Person Person02 = new Person("Bob Thornley", 25, 171, 68.2);
 
         //Output to screen...
 
         //Person1
+        //age James...
+        Person01.growOlder();
         System.out.println("Person1: " + Person01.getName() + " " + Person01.getAge() +
-                " " + Person01.getHeight() + " " + Person01.getWeight());
+                " " + Person01.getHeight() + "cm " + Person01.getWeight() + "lbs");
 
         //Person2
         System.out.println("Person2: " + Person02.getName() + " " + Person02.getAge() +
-                " " + Person02.getHeight() + " " + Person02.getWeight());
+                " " + Person02.getHeight() + "cm " + Person02.getWeight() + "lbs");
     }
 }
